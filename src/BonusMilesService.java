a94f23d4258f163adc05d0cf1f69b19b60e14e1d
Татьяна Тарасова <kryachkovat@gmail.com>
2022-07-17 14:16:59 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+
+            public int calculate(int cost) {
+                int milesAmount = cost / 20;
+
+                return milesAmount;
+            }
+}
